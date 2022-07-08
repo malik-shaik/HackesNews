1db8e4056f376ef5f72b2f7a7b3eb9590cf4e774
malik <malik.shaik@outlook.com>
2022-07-08 21:12:19 +0200

    added Navbar

---
+++ src/components/navbar/nav-links/index.tsx
@@ -0,0 +1,51 @@
+import { FC, useEffect, useState } from "react";
+import { ToggleButtonProps } from "../toggle";
+
+type WindowSize = {
+  width: number;
+  height: number;
+};
+
+type NavLinksProps = ToggleButtonProps;
+
+export const NavLinks: FC<NavLinksProps> = ({ isMenuOpen, setMenuOpen }) => {
+  const [size, setSize] = useState<WindowSize>({
+    height: 0,
+    width: 0,
+  });
+
+  useEffect(() => {
+    const handleResize = () => {
+      setSize({
+        width: window.innerWidth,
+        height: window.innerHeight,
+      });
+    };
+    window.addEventListener("resize", handleResize);
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (size.width > 768 && isMenuOpen) {
+      setMenuOpen(false);
+    }
+  }, [size.width, isMenuOpen]);
+
+  const isMenuClass = isMenuOpen && size.width < 768 ? "isMenu" : "";
+
+  const handleNavLinkClick = () => {
+    if (isMenuOpen) {
+      setMenuOpen(false);
+    }
+  };
+
+  return (
+    <nav className={`navbar__nav ${isMenuClass}`}>
+      <ul>
+        <li onClick={handleNavLinkClick}>New Stories</li>
+        <li onClick={handleNavLinkClick}>Contact us</li>
+      </ul>
+    </nav>
+  );
+};
---
+++ src/lib/getImage.ts
@@ -0,0 +1,19 @@
+export enum ImageType {
+  LOGO = "logo",
+  STORY = "story",
+}
+type Params = {
+  imageType: ImageType;
+};
+
+/**
+ * Gives image url based on type
+ * @param imageType which can be 'logo' or 'story'
+ * @returns image url
+ */
+export const getImage = ({ imageType }: Params): string => {
+  if (imageType === ImageType.LOGO) {
+    return "images/hacker-logo.png";
+  }
+  return "";
+};
