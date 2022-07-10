d499b7d98776d7052b208b95738afff7bae4f20b
malik <malik.shaik@outlook.com>
2022-07-10 12:05:40 +0200

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import "styles/_main.scss";
-import { Navbar } from "components/navbar";
+import React from 'react'
+import 'styles/_main.scss'
+import { Navbar } from 'components/navbar'
 
 function App() {
   return (
     <>
       <Navbar />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/components/navbar/logo/index.tsx
+++ src/components/navbar/logo/index.tsx
@@ -1,13 +1,13 @@
-import { FC } from "react";
-import { getImage, ImageType } from "lib/getImage";
-const { LOGO } = ImageType;
+import { FC } from 'react'
+import { getImage, ImageType } from 'lib/getImage'
+const { LOGO } = ImageType
 
 export const Logo: FC = () => {
-  const logoUrl = getImage({ imageType: LOGO });
+  const logoUrl = getImage({ imageType: LOGO })
   return (
     <div className="navbar__logo">
       <img src={logoUrl} alt="logo" height={50} />
       <h3>Hackers News</h3>
     </div>
-  );
-};
+  )
+}
