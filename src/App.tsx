128d79acc93e8150db35b3fefe9d46cc8d50a2bb
malik <malik.shaik@outlook.com>
2022-07-11 16:07:58 +0200

    added contact page

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+
 import 'styles/_main.scss'
 import { Navbar } from 'components/navbar'
 import { LandingPage } from 'components/landing-page'
+import { ContactPage } from 'components/contact-page'
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Navbar />
-      <LandingPage />
-    </>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="contact" element={<ContactPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
---
+++ src/components/contact-page/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const ContactPage = () => {
+  return (
+    <div>
+      <div className="contact">
+        <h3>Contact us</h3>
+        <p>+1 (234) 567-891</p>
+        <p>+1 (234) 567-891</p>
+        <p>contact@headoffice.com</p>
+      </div>
+
+      <div className="contact">
+        <h3>Location</h3>
+        <p>121 Rock Street, 21 Avenue</p>
+        <p>New York, NY 92103-90000</p>
+      </div>
+    </div>
+  )
+}
